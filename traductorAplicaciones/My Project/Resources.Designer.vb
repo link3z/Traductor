dd84343392fda891ff24869991015bea025a6e30
link3z <info@recompila.com>
2014-12-01 23:11:44 +0100

    Limpieza del proyecto

--- traductorAplicaciones/My Project/Resources.Designer.vb
+++ traductorAplicaciones/My Project/Resources.Designer.vb
@@ -59,35 +59,5 @@ Namespace My.Resources
                 resourceCulture = value
             End Set
         End Property
-        
-        '''<summary>
-        '''  Busca un recurso adaptado de tipo System.Drawing.Bitmap.
-        '''</summary>
-        Friend ReadOnly Property a_enlace_161() As System.Drawing.Bitmap
-            Get
-                Dim obj As Object = ResourceManager.GetObject("a_enlace_161", resourceCulture)
-                Return CType(obj,System.Drawing.Bitmap)
-            End Get
-        End Property
-        
-        '''<summary>
-        '''  Busca un recurso adaptado de tipo System.Drawing.Bitmap.
-        '''</summary>
-        Friend ReadOnly Property lenguaje() As System.Drawing.Bitmap
-            Get
-                Dim obj As Object = ResourceManager.GetObject("lenguaje", resourceCulture)
-                Return CType(obj,System.Drawing.Bitmap)
-            End Get
-        End Property
-        
-        '''<summary>
-        '''  Busca un recurso adaptado de tipo System.Drawing.Bitmap.
-        '''</summary>
-        Friend ReadOnly Property lenguaje_32() As System.Drawing.Bitmap
-            Get
-                Dim obj As Object = ResourceManager.GetObject("lenguaje_32", resourceCulture)
-                Return CType(obj,System.Drawing.Bitmap)
-            End Get
-        End Property
     End Module
 End Namespace
